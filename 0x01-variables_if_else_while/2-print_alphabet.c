b0e8a7b8cf23df09d5a1c7ed6fa569babba72061
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-18 04:56:06 -0700

    task-2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - to print alpahbet a-z in lower case
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
+
+
