b964d0dcd2818293c2df68a95b43622482e63990
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-18 05:09:38 -0700

    task-3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - to prrint alphabet in both lowercase and uppercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putch(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
