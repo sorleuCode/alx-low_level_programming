a6ae8f1c506467485dcaa1f3b502595dac739160
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-18 06:33:03 -0700

    task-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the ten digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+		}
