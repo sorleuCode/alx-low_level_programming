f1c40d52c49eba8677087828eefcff7f6c2184e0
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-18 07:06:03 -0700

    task-6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all the ten digits using putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
