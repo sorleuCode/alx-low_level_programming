66b8b5c4fca658f31257b8853bde0e37978c4dff
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-18 08:05:46 -0700

    task-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination of single_digit numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+
+		if (a != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
