c76e30c26b6e592aec6449a05794d036e257bec7
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-20 08:55:43 -0700

    task-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = a; lc <= z, lc++)
+	{
+		_putchar(lc);
+	}
+	_putchar('\n');
+}
