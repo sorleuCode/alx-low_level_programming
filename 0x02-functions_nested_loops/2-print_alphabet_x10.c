03ac5e13e6cadad4f0c645eedf004c8c86e3775e
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-20 09:24:07 -0700

    task-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 	char lc;
 	int ten;
 
-	for (lc = 'a'; lc <= 'z'; lc++)
+	for (ten = 0; ten <= 9; ten++)
 	{
-		for (ten = 0; ten <= 9; ten++)
+		for (lc = 'a'; lc <= 'z'; lc++)
 		{
 			_putchar(lc);
 		}
