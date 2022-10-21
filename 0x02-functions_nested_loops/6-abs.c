12c6a7cf1c60552072b69eb8b3d848cf1a3d84af
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-21 02:34:49 -0700

    task-6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of a number
+ * @x: integer to be printed
+ * Return: Alwyas 0
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+	{
+		return (x);
+	}
+	else
+		return (-x);
+}
