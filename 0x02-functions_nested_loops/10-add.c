a058ae8e3b7fdcf279b9b88c40005fc3c88f4f64
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-21 07:43:26 -0700

    task-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - print  two integers
+ * @a: first number
+ * @b: second number
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int ad;
+
+	ad = a + b;
+
+	_putchar(ad + '0');
+	return (ad);
+}
