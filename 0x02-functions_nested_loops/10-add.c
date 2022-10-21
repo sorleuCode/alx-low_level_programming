1c2498a79cf901667deaefe3aa9407b9a0f388e9
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-21 11:45:48 -0700

    task-10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -11,15 +11,5 @@ int add(int a, int b)
 	int ad;
 
 	ad = a + b;
-
-	if (ad <= 9)
-	{
-		_putchar(ad + '0');
-	}
-	else
-	{
-		_putchar((ad / 10) + '0');
-		_putchar((ad % 10) + '0');
-	}
 	return (ad);
 }
