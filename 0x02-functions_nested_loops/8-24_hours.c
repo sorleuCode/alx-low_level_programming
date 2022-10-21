041d6a8ffed886f94f2b2b74e86021001c3f0148
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-21 03:57:25 -0700

    task-8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar(hr + '0');
-			_putchar(hr + '0');
+			_putchar((hr / 10)  + '0');
+			_putchar((hr % 10) + '0');
 			_putchar(':');
-			_putchar(min + '0');
-			_putchar(min + '0');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
 	}
