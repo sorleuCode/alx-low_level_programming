6950b0897f562d47be64d806cbefa180bd5607be
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-21 06:48:35 -0700

    task-9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,25 +10,24 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		_putchar('0');
-
-		for (multi = 1; multi <= 9; multi++)
+		for (multi = 0; multi <= 9; multi++)
 		{
-			_putchar(',');
-			_putchar(' ');
-
 			val = num * multi;
 
 			if (val <= 9)
 			{
-				_putchar(' ');
 				_putchar(val + '0');
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 
 			}
 			else
 			{
 				_putchar((val / 10) + '0');
 				_putchar((val % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
