2f3a9000eade6362357424b64213e968002c8588
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-21 05:11:50 -0700

    task-9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - prints the nine timestable
+ * Return: Always 0
+ */
+void times_table(void)
+{
+	int num, multi, val;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (multi = 1; multi <= 9; multi++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			val = num * multi;
+
+			if (val <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((val / 10) + '0');
+				-putchar((val % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
