93e7a8052cbbd3cbbdd3f41ee466d0741e3687ae
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-25 04:52:02 -0700

    c-debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,17 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-int _putchar(char c);
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+int convert_day(int month, int day);
 void print_times_table(int n);
+
+
 #endif
