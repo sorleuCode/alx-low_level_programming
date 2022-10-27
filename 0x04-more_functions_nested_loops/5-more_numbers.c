d0a7a5801aa69e5bd650db555051e91015b21848
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-26 17:43:35 -0700

    task-5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,11 +4,11 @@
  * more_numbers - prints nunbers 0 to 14 ten times
  */
 
-void more_numbers(void)
+void more_numbers(voiid)
 {
 	int i, j;
 
-	for (j = 0; j <= 0; j++)
+	for (j = 0; j <= 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
