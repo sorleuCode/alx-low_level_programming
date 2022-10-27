447bd13f774e37ce7fb35f467bb00621f40af049
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-27 13:36:14 -0700

    task-10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int space, xter;
+
+	if (size > 0)
+	{
+		for (xter = 0; xter < size; xter++)
+		{
+			for (space = size - xter; space > 0; space--)
+			{
+				_putchar(' ');
+			}
+			_putchar('#');
+			if (xter == size - 1)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
+
