bb88b0218b322292111c10045653ec17727770aa
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-26 18:28:46 -0700

    task-5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 10; j++)
+	for (j = 1; j <= 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i >= 10)
+			if (i >= 10 && i <= 14)
 			{
-				_putchar((i / 10) + '0');
+				_putchar('i');
 				_putchar((i % 10) + '0');
 			}
 		}
