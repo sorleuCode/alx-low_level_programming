6f1994ca20ac6ea62958907dc3f60720f51704cf
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-27 04:20:31 -0700

    task-5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,9 @@ void more_numbers(void)
 		{
 			if (i >= 10)
 			{
-				_putchar('i');
-				_putchar(i % 10 + '0');
+				_putchar((i / 10) + '0');
 			}
+			_putchar((i % 10) + '0');
 		}
 		_putchar('\n');
 	}
