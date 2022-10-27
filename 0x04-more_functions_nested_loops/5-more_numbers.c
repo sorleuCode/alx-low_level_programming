2f79ca06e647e1d40f5c7ec599e6a998210fb8f6
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-26 17:40:46 -0700

    task-5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints nunbers 0 to 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 0; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i + '0');
+		}
+
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
