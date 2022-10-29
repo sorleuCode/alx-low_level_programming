31c8b27c8a61a51fa09c957e8ae4cabfc31531d2
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-29 08:20:53 -0700

    task-5

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints to the stdout, one character out of two
+ * @str: the character to be printed
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
