3da737d5b8208b0b08241731e1ab1ebe8ccab9e4
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-29 04:55:13 -0700

    task-3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - peints string to the stdout
+ * @str: the string to be printed
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		*str++;
+
+		_putchar('*str++');
+	}
+	_putchar('\n');
+}
