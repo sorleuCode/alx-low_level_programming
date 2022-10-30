51a47550eaf463c32822a3121fd51cd4d0745383
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-10-29 19:18:13 -0700

    task-2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @src: source
+ * @dest: destination
+ * @n: the memory size
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+	return (dest);
+}
