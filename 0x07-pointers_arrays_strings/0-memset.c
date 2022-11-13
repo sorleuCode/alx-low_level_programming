090f8b26bfdeb139015f081e2e22c3b610cff07a
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-11-13 03:59:14 -0800

    c pointers and array

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
- * @s: array of characters
- * @b: each character
+ * _memset - prints the first n byte of tye memory to the std out
+ * @s: array of character
+ * @b: the character to print
  * @n: number of byte
- * Return: return pointer
+ * Return: return pointer to the to tye s  memory area
  */
 
 char *_memset(char *s, char b, unsigned int n)
