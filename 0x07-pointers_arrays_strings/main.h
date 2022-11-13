8a2349704bffe4237cc0b26fba08ba4ce913d94f
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-11-13 04:07:45 -0800

    c pointers and arrays

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stddef.h>
+
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
