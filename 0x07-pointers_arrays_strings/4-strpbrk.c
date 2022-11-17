0b6bb1081317c6dc9218b32a45abf779206d8bad
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-11-17 03:11:37 -0800

    task-4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,15 +9,16 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept[j])
+			if (*s == accept[j])
 				return (s);
 		}
+		s++;
 	}
 	return (NULL);
 }
