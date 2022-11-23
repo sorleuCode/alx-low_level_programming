53ff0e56ee93ce2e451d57cbbaa69f86f2e4bdd1
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-11-23 14:00:48 -0800

    task-1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * Return: 0 always
  */
 
-int main(unsigned int argc, __attribute__ ((__unused__)) char *argv[])
+int main(int argc, __attribute__ ((__unused__)) char *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
