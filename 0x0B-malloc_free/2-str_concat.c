5b445d9e35451d5f385e62486c7500622d9ab7d0
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2022-11-28 02:23:28 -0800

    task-2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,8 +31,6 @@ char  *str_concat(char *s1, char *s2)
 	for (index = 0; s1[index]; index++)
 		concat_strn[concat_index++] = s1[index];
 	for (index = 0; s2[index]; index++)
-		if (s2 == " ")
-			s2 = NULL;
 		concat_strn[concat_index] = s2[index];
 	return (concat_strn);
 }
